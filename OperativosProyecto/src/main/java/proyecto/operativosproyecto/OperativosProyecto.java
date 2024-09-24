4b676d6fd3c4223d6014ac8f28091212d8f381ed
AugustoSisiruca <sisiruca.augusto@correo.unimet.edu.ve>
2024-09-24 11:47:13 -0400

    IniciarCambios 🗿

---
+++ OperativosProyecto/src/main/java/proyecto/operativosproyecto/OperativosProyecto.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package proyecto.operativosproyecto;
+
+/**
+ *
+ * @author sisir
+ */
+public class OperativosProyecto {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
