99b936a36270232c947c0a69152ecb1eab783ee7
AugustoSisiruca <sisiruca.augusto@correo.unimet.edu.ve>
2024-10-07 10:07:38 -0400

    Primeros Cambios 🤹

---
+++ OperativosProyecto/src/main/java/proyecto/operativosproyecto/Empleado.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyecto.operativosproyecto;
+
+/**
+ *
+ * @author sisir
+ */
+
+/*
+Numero de Empleados para HP (Augusto 20221110152): 2 + 12 = 14
+Numero de Empleados para Dell (Angel
+*/
+
+public class Empleado {
+    
+
+}
